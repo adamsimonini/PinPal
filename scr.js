b69a2d7571a06787d8e06ec0f0aac695101a50c4
Adam Simonini <adam.simonini@gmail.com>
2017-07-20 18:37:23 -0400

    Updated Success and Restart Buttons

--- scr.js
+++ scr.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
 
-  var pinArray = [];
+  var initialArray = [];
+  var pin = [];
+  var compareArray = [];
   var numArray = ["1","2","3","4","5","6","7","8","9","*","0","#"]
 
+// Simple function for clearing initArray * #seqience
+  function clearValues(){
+    initialArray = [];
+    document.getElementById('sequence').innerHTML = '';
+  }
+
+// Various Button Functions
   $(".numButton").on("click", function(){
-    pinArray.push($(this).attr('id'));
-    console.log(pinArray);
-    document.getElementById("sequence").innerHTML = pinArray.toString();
+    initialArray.push($(this).attr('id'));
+    document.getElementById("sequence").innerHTML = initialArray.join(' ').toString();
   });
-});
-$("#submit").on("click", function(){
-  console.log("fucker");
-  document.getElementById("sequence").innerHTML = "";
+
+  $('#submit').on("click", function(){
+    if(initialArray.length === 0){
+        alert("Please enter a sequence of numbers to test");
+    }else{
+      pin = initialArray;
+      $("#submit").addClass("disabled");
+      clearValues();
+    }
+  });
+
+  $('#restart').on("click", function(){
+    $("#submit").removeClass("disabled");
+    clearValues();
+  });
+
 });
