b2ed642026ffadce1117ffa249caf7764e5cc136
Adam Simonini <adam.simonini@gmail.com>
2017-07-21 13:15:23 -0400

    Pin and CompareArray are now compared resulting in a boolean value

--- scr.js
+++ scr.js
@@ -5,31 +5,62 @@ $(document).ready(function(){
   var compareArray = [];
   var numArray = ["1","2","3","4","5","6","7","8","9","*","0","#"]
 
+  function compare(a,b){
+    if(a.toString() == b.toString()){
+      return true
+    }else{
+      return false;
+    }
+  }
+
 // Simple function for clearing initArray * #seqience
   function clearValues(){
     initialArray = [];
+    compareArray = [];
+    if(!$("#submit").hasClass("unsubmitted")){
+      $("#submit").hasClass("unsubmitted");
+    }
     document.getElementById('sequence').innerHTML = '';
   }
 
 // Various Button Functions
   $(".numButton").on("click", function(){
-    initialArray.push($(this).attr('id'));
-    document.getElementById("sequence").innerHTML = initialArray.join(' ').toString();
-  });
-
-  $('#submit').on("click", function(){
-    if(initialArray.length === 0){
-        alert("Please enter a sequence of numbers to test");
+    if($("#submit").hasClass("unsubmitted")){
+      initialArray.push($(this).attr('id'));
+      document.getElementById("sequence").innerHTML = initialArray.join(' ').toString();
     }else{
-      pin = initialArray;
-      $("#submit").addClass("disabled");
-      clearValues();
+      if($("#submit").hasClass("unsubmitted") && compareArray.length !== 0){
+        document.getElementById("submit").removeClass("disabled");
+      }
+      compareArray.push($(this).attr('id'));
+      document.getElementById("sequence").innerHTML = compareArray.join(' ').toString();
     }
   });
 
+  $('#submit').on("click", function(){
+    if(initialArray.length === 0 && $("#submit").hasClass("unsubmitted"))
+    {
+      alert("Please enter an initial sequence of numbers");
+    }else if(compareArray.length === 0 && !$("#submit").hasClass("unsubmitted")){
+      alert("Pleasetry to re-enter the submitted sequence");
+    }else if(initialArray.length !== 0 && $("#submit").hasClass("unsubmitted")){
+      pin = initialArray;
+      console.log(pin);
+      $("#submit").removeClass("unsubmitted");
+      $("#submit").addClass("disabled");
+      clearValues();
+    }else if (compareArray.length > 0){
+      console.log(pin);
+      console.log(compareArray);
+      alert(compare(pin, compareArray));
+    }
+
+  });
   $('#restart').on("click", function(){
     $("#submit").removeClass("disabled");
     clearValues();
+    console.log(compareArray);
+    console.log(initialArray);
   });
 
 });
