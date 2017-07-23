3930b3cae00538ee2bc4efe5fe89665cbbd0c590
Adam Simonini <adam.simonini@gmail.com>
2017-07-23 13:21:08 -0400

    Reset Button Working

--- scr.js
+++ scr.js
@@ -4,7 +4,28 @@ $(document).ready(function(){
   var pin = [];
   var compareArray = [];
   var success = 0;
-  var numArray = ["1","2","3","4","5","6","7","8","9","asterisk","0","pound"];
+  const numArray = ["1","2","3","4","5","6","7","8","9","asterisk","0","pound"];
+  const numArray2 = ["1","2","3","4","5","6","7","8","9","asterisk","0","pound"];
+
+
+  // Remove all dynamically generated divs;
+  function clearDivs(){
+    for(i=1;i<5;i++){
+      var div = document.getElementById('row' + i);
+      while(div.firstChild){
+        div.removeChild(div.firstChild);
+      }
+    }
+  }
+    // Simple function for clearing initArray * #seqience
+function clearValues(){
+  initialArray = [];
+  compareArray = [];
+  if(!$("#submit").hasClass("unsubmitted")){
+    $("#submit").hasClass("unsubmitted");
+  }
+  document.getElementById('sequence').innerHTML = '';
+}
 
   function FisherYatesShuffle(array){
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -21,36 +42,46 @@ $(document).ready(function(){
     return array;
   }
 
-  function specialCharacter(char){
+  function specialCharacter(character){
     var found = "";
-    char === "asterisk" ? found = "*" : found = "#";
+    character === "asterisk" ? found = "*" : found = "#";
     return found;
   }
+  //Add listened to Divs, each time they are dynamically created
+  function addClickResponse(){
+    var numberButtons = document.getElementsByClassName("numButton");
 
-  function generateGrid(){
+    var addListeners = function() {
+      if($("#submit").hasClass("unsubmitted")){
+        initialArray.push($(this).attr('id'));
+        document.getElementById("sequence").innerHTML = initialArray.join(' ').toString();
+      }else{
+        compareArray.push($(this).attr('id'));
+        document.getElementById("sequence").innerHTML = compareArray.join(' ').toString();
+      }
+    };
+    for (var i = 0; i < numberButtons.length; i++) {
+        numberButtons[i].addEventListener('click', addListeners, false);
+    }
+  }
+
+  function generateGrid(array){
     var k = 0;
     for(i=1;i<5;i++){
       var currentRow = $("#row" + i);
       for(j=0;j<3;j++){
         var newKey = document.createElement("div");
-        newKey.id = numArray[k];
+        newKey.id = array[k];
         newKey.setAttribute("class", "col-xs-4");
-        newKey.innerHTML = '<button id="' + numArray[k] +
+        newKey.innerHTML = '<button id="' + array[k] +
         '" type="button" class="btn numButton btn-block btn-lg">' +
-        (!isNaN(parseInt(numArray[k])) ? numArray[k] : specialCharacter(numArray[k])) + '</button>';
+        (!isNaN(parseInt(array[k])) ? array[k] : specialCharacter(array[k])) + '</button>';
         $(currentRow).append(newKey);
         k++;
       }
     }
+    addClickResponse();
   }
-
-  function clearDivs(){
-    for(i=1;i<5;i++){
-      $('#row' + i).html('');
-    }
-  }
-
-  generateGrid();
 
   function compare(a,b){
     if(a.toString() == b.toString()){
@@ -63,38 +94,17 @@ $(document).ready(function(){
     }
   }
 
-// Simple function for clearing initArray * #seqience
-  function clearValues(){
-    initialArray = [];
-    compareArray = [];
-    if(!$("#submit").hasClass("unsubmitted")){
-      $("#submit").hasClass("unsubmitted");
-    }
-    document.getElementById('sequence').innerHTML = '';
-  }
-
-// Various Button Functions
-  $(".numButton").on("click", function(){
-    if($("#submit").hasClass("unsubmitted")){
-      initialArray.push($(this).attr('id'));
-      document.getElementById("sequence").innerHTML = initialArray.join(' ').toString();
-    }else{
-      compareArray.push($(this).attr('id'));
-      document.getElementById("sequence").innerHTML = compareArray.join(' ').toString();
-    }
-  });
-
   $('#submit').on("click", function(){
     if(initialArray.length === 0 && $("#submit").hasClass("unsubmitted"))
     {
       alert("Please enter an initial sequence of numbers");
     }else if(compareArray.length === 0 && !$("#submit").hasClass("unsubmitted")){
-      alert("Pleasetry to re-enter the submitted sequence");
+      alert("Please try to re-enter the submitted sequence");
     }else if(initialArray.length !== 0 && $("#submit").hasClass("unsubmitted")){
       pin = initialArray;
-      numArray = FisherYatesShuffle(numArray);
+      var randomArray = FisherYatesShuffle(numArray);
       clearDivs();
-      generateGrid();
+      generateGrid(randomArray);
       $("#submit").removeClass("unsubmitted");
       clearValues();
     }else if (compareArray.length > 0){
@@ -107,9 +117,12 @@ $(document).ready(function(){
 
   $('#restart').on("click", function(){
     $("#submit").addClass("unsubmitted");
+    clearDivs();
     clearValues();
     success = 0;
     $("#successContainer").addClass("hidden");
+    generateGrid(numArray2);
   });
 
+  generateGrid(numArray);
 });
